eba482d784474d72ab81f0a0cdc261d6c2f3e828
BRUNO NAHUEL MARASI <48113202@ort>
2025-04-11 10:02:52 -0300

    terminamosPracitca

---
+++ index.js
@@ -0,0 +1,48 @@
+import express from "express";
+
+const app = express()
+const port = 3000
+
+const usuario = {
+    nombre: "Ana",
+    edad: 25
+}
+const materias = [
+    {
+    nombre: "Matematica"
+    },
+    {
+    nombre: "Lengua"
+    }
+]
+const productos = ["Mouse", "Teclado", "Monitor"]
+
+app.get('/', (req, res) => {
+    res.send('Bienvenido a mi servidor');
+})
+
+app.get('/saludo', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.get('/numero', (req, res) => {
+    res.send('42');
+})
+
+app.get('/usuario', (req, res) => {
+    res.send(usuario);
+})
+
+app.get('/productos', (req, res) => {
+    res.send(productos);
+})
+
+app.get('/materias', (req, res) => {
+    res.send(materias);
+})
+
+app.listen(port, () => { 
+    console.log(`Listening on http://localhost:${port}`)
+})
+
+
